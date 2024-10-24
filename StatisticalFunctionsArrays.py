1597bb34e0c4828d569257b1e24738f5771eaab9
Douglas Cortes <135165192+dxara10@users.noreply.github.com>
2024-10-24 17:12:33 -0300

    Create StatisticalFunctionsArrays.py

---
+++ StatisticalFunctionsArrays.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Criar um array NumPy
+arr = np.array([1, 2, 3, 4, 5])
+
+# Média
+media = np.mean(arr)
+
+# Desvio padrão
+desvio_padrao = np.std(arr)
+
+print("Média:", media)
+print("Desvio Padrão:", desvio_padrao)
