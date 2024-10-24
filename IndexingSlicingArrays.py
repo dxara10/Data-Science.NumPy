d61b3918ddbf434bd5645e4112a83b9dfed6fec3
Douglas Cortes <135165192+dxara10@users.noreply.github.com>
2024-10-24 17:11:25 -0300

    Create IndexingSlicingArrays.py

---
+++ IndexingSlicingArrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Criar um array NumPy
+arr = np.array([1, 2, 3, 4, 5])
+
+# Acessar elementos por índice
+print(arr[0])  # Primeiro elemento
+print(arr[-1]) # Último elemento
+
+# Fatiamento
+print(arr[1:4])  # Elementos do índice 1 ao 3
